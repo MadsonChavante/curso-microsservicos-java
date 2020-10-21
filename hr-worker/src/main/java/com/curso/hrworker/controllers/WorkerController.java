661842772fa4c05c986fb340540428d0eaf2915a
Madson <ml.chavante@gmail.com>
2020-10-21 16:55:52 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/curso/hrworker/controllers/WorkerController.java
@@ -0,0 +1,34 @@
+package com.curso.hrworker.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.hrworker.entities.Worker;
+import com.curso.hrworker.repositories.WorkerRepository;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerController {
+
+	@Autowired
+	private WorkerRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll() {
+		List<Worker> list = repository.findAll();
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		Worker worker = repository.findById(id).get();
+		return ResponseEntity.ok(worker);
+	}
+
+}
