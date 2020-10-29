9b55c129d85d01f1d5343cac09f0854afbc132d0
Madson <ml.chavante@gmail.com>
2020-10-29 08:44:47 -0300

    init eureka server

---
+++ hr-eureka-server/src/test/java/com/curso/hrpayroll/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
